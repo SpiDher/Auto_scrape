3c71a202fa983e856d410df03e605bed4ab6a2fd
SpiDher <penivera655@gmail.com>
2024-10-29 16:06:00 +0100

    Attachment attached succesfully

--- scrape.py
+++ scrape.py
@@ -7,9 +7,11 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
+import csv
 
 def main():
     spread(get_jobs())
+    send_email(**email_params)
 
 base_url = 'https://remoteok.com/api/'
 user_agent = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36'
@@ -18,6 +20,13 @@ request_header = {
     'user-agent': user_agent,
     'language': 'en-us, en;q=0.5'
 }
+def read_login():
+    with open('login.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        for values in reader:
+            return (values['email'],values['password'])
+        
+            
 
 def get_jobs():
     response = requests.get(base_url,headers = request_header)
@@ -46,9 +55,9 @@ def spread(response):
 email_params = {
     'send_to':['penivera655@gmail.com'],
     'send_from': 'charlesocean2023@gmail.com',
-    'subject':'TEstin smtp servers',
+    'subject':'Testing smtp servers',
     'text': 'This is a simple test of gmail smtp server',
-    'files': None
+    'files': ['remoteok.xls']
     }
 def send_email(send_to,send_from,subject,text,files):
     assert isinstance(send_to,list)
@@ -56,7 +65,24 @@ def send_email(send_to,send_from,subject,text,files):
     msg['From']= send_from
     msg['To']= COMMASPACE.join(send_to)
     msg['Date']= formatdate(localtime=True)
-    smtp = smtplib.SMTP('smtp.gmail.com: 587')
-    smtp.starttls()
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text))
+    if files:
+        for file in files:
+            with open(file, 'rb') as attachment:
+                part = MIMEApplication(attachment.read(), name=basename(file))
+            part['Content-Disposition'] = f'attachment; filename="{basename(file)}"'
+            msg.attach(part)
+    
+    try:
+        smtp = smtplib.SMTP('smtp.gmail.com: 587')
+        smtp.starttls()
+        smtp.login(read_login()[0],read_login()[1])
+        smtp.sendmail(send_from,send_to,msg.as_string())
+        print('Succesful')
+    except Exception as e:
+        print(f'failed to send {str(e)}')
+    
+    
 if __name__ == '__main__':
     main()
